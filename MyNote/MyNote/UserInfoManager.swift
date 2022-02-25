d0f1a30f780da09faadbb773c14aebf942180413
lee02029 <kkang47140@gmail.com>
2022-02-25 21:18:31 +0900

    220225

---
+++ MyNote/MyNote/UserInfoManager.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfoManager.swift
+//  MyNote
+//
+//  Created by Yoonjae lee on 2022/02/25.
+//
+
+import UIKit
+
+struct UserInfoKey {
+    static let loginId = "LoginId"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = 
+}
