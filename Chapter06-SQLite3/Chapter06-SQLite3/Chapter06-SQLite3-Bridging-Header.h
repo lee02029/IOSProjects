56154dc04d7c7ccc03acecb96ae79c11e1299a86
lee02029 <kkang47140@gmail.com>
2022-03-01 17:37:53 +0900

    220301

---
+++ Chapter06-SQLite3/Chapter06-SQLite3/Chapter06-SQLite3-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Chapter06-SQLite3-Bridging-Header.h
+//  Chapter06-SQLite3
+//
+//  Created by Yoonjae lee on 2022/03/01.
+//
+
+#import <sqlite3.h>
--- Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var db: OpaquePointer? = nil
+        var stmt: OpaquePointer? = nil
+        
+//        let filedb = FileManager()
+//        let docPathURL = fileMgr.urls(for: .documentDirectory, in:.userDomainMask).first!
+//        let dbPath = docPathURL.appendingPathComponent("db.sqlite")?.path
+        let dbPath = "/Users/yoonjae/db.sqlite"
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
+        if sqlite3_open(dbPath, &db) == SQLITE_OK {
+            if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK {
+                if sqlite3_step(stmt) == SQLITE_DONE {
+                    print("Create Table Success")
+                }
+                sqlite3_finalize(stmt)
+            } else {
+                print("Prepare Statement Fail")
+            }
+            sqlite3_close(db)
+        } else {
+            print("Datebase Connect Fail")
+            return
+        }
     }
-
-
 }
 
